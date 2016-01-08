088ceb898a22142044f4738e3a5747bc5c132e5b
Tom Bell <tomb@tomb.io>
2016-01-08 22:23:25 +0000

    Add status item and menu setup code

--- Stark/AppDelegate.swift
+++ Stark/AppDelegate.swift
@@ -2,12 +2,32 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
+
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         AccessibilityHelper.askForAccessibilityIfNeeded()
+
+        setupStatusItem()
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
 
     }
-}
 
+    func setupStatusItem() {
+        let image = NSImage(named: "StatusItemIcon")
+        image?.template = true
+
+        statusItem.highlightMode = true
+        statusItem.image = image
+
+        let menu = NSMenu()
+        menu.addItemWithTitle("Quit Stark", action: "quit:", keyEquivalent: "")
+
+        statusItem.menu = menu
+    }
+
+    func quit(sender: AnyObject?) {
+        NSApp.terminate(nil)
+    }
+}
