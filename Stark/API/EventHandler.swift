6c78e51938cc745d1f8372fb7867132c57b33a0b
Tom Bell <tomb@tomb.io>
2016-02-18 01:57:53 +0000

    Add EventHandler and Stark.on function to API

--- Stark/API/EventHandler.swift
+++ Stark/API/EventHandler.swift
@@ -2,9 +2,47 @@ import AppKit
 import JavaScriptCore
 
 @objc protocol EventHandlerJSExport: JSExport {
-
+    var name: String { get }
 }
 
 public class EventHandler: Handler, EventHandlerJSExport {
+    public var name: String
 
+    private var notification: String
+    private var notificationCenter: NSNotificationCenter
+
+    init(event: String, callback: JSValue) {
+        self.name = event
+
+        self.notification = EventHelper.notificationForEvent(event)
+        self.notificationCenter = EventHelper.notificationCenterForNotification(self.notification)
+
+        super.init()
+
+        self.manageCallback(callback)
+
+        self.notificationCenter.addObserver(self, selector: "didReceiveNotification:", name: self.notification, object: nil)
+    }
+
+    deinit {
+        self.notificationCenter.removeObserver(self, name: self.notification, object: nil)
+    }
+
+    func didReceiveNotification(notification: NSNotification) {
+        guard let userInfo = notification.userInfo else {
+            self.call()
+            return
+        }
+
+        if let runningApp = userInfo[NSWorkspaceApplicationKey] as? NSRunningApplication {
+            let app = Application(app: runningApp)
+            self.callWithArguments([app])
+            return
+        }
+
+        if let window = userInfo[AppObserverWindowKey] as? Window {
+            self.callWithArguments([window])
+            return
+        }
+    }
 }
--- Stark/API/Stark.swift
+++ Stark/API/Stark.swift
@@ -5,6 +5,7 @@ import JavaScriptCore
     func log(message: String)
     func reload()
     @objc(bind:::) func bind(key: String, modifiers: [String], callback: JSValue) -> KeyHandler
+    @objc(on::) func on(event: String, callback: JSValue) -> EventHandler
 }
 
 public class Stark: NSObject, StarkJSExport {
@@ -24,5 +25,9 @@ public class Stark: NSObject, StarkJSExport {
 
     @objc(bind:::) public func bind(key: String, modifiers: [String], callback: JSValue) -> KeyHandler {
         return config.bindKey(key, modifiers: modifiers, callback: callback)
+    }
+
+    @objc(on::) public func on(event: String, callback: JSValue) -> EventHandler {
+        return EventHandler(event: event, callback: callback)
     }
 }
