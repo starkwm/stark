5b2677c1fe14f076c988ef33b9a41db4eade72d2
Tom Bell <tomb@tomb.io>
2016-03-28 17:18:09 +0100

    Add file logging helper

---
+++ Stark/Helpers/LogHelper.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+public class LogHelper {
+    public static func log(message: String) {
+        let dir = NSURL(fileURLWithPath: NSHomeDirectory())
+        let file = dir.URLByAppendingPathComponent(".stark.log")
+
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "[yyyy-MM-dd HH:mm:ss]"
+
+        let log = String(format: "%@ %@", formatter.stringFromDate(NSDate()), message)
+
+        try! self.stringAppendLineToURL(log, fileURL: file)
+    }
+
+    private static func stringAppendLineToURL(message: String, fileURL: NSURL) throws {
+        try self.stringAppendToURL(message.stringByAppendingString("\n"), fileURL: fileURL)
+    }
+
+    private static func stringAppendToURL(message: String, fileURL: NSURL) throws {
+        let data = message.dataUsingEncoding(NSUTF8StringEncoding)!
+        try self.dataAppendToURL(data, fileURL: fileURL)
+    }
+
+    private static func dataAppendToURL(data: NSData, fileURL: NSURL) throws {
+        if let fileHandle = try? NSFileHandle(forWritingToURL: fileURL) {
+            defer {
+                fileHandle.closeFile()
+            }
+
+            fileHandle.seekToEndOfFile()
+            fileHandle.writeData(data)
+        }
+        else {
+            try data.writeToURL(fileURL, options: .DataWritingAtomic)
+        }
+    }
+}
