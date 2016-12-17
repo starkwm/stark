28a2a48dd68b45597bfc6404736f957ca66a50cd
Tom Bell <tomb@tomb.io>
2016-12-17 00:03:12 +0000

    Refactor KeyCodeHelper class
    Refactor the switch/case statement into a Dictionary of String:Int. This
    helps reduce the cyclomatic complexity and function body length of the
    `keyCodeForString` function.

--- Stark/Helpers/KeyCodeHelper.swift
+++ Stark/Helpers/KeyCodeHelper.swift
@@ -1,30 +1,88 @@
 import Carbon
 
+fileprivate let relocatableKeyCodes = [
+    kVK_ANSI_A, kVK_ANSI_B, kVK_ANSI_C, kVK_ANSI_D, kVK_ANSI_E,
+    kVK_ANSI_F, kVK_ANSI_G, kVK_ANSI_H, kVK_ANSI_I, kVK_ANSI_J,
+    kVK_ANSI_K, kVK_ANSI_L, kVK_ANSI_M, kVK_ANSI_N, kVK_ANSI_O,
+    kVK_ANSI_P, kVK_ANSI_Q, kVK_ANSI_R, kVK_ANSI_S, kVK_ANSI_T,
+    kVK_ANSI_U, kVK_ANSI_V, kVK_ANSI_W, kVK_ANSI_X, kVK_ANSI_Y,
+    kVK_ANSI_Z,
+
+    kVK_ANSI_0, kVK_ANSI_1, kVK_ANSI_2, kVK_ANSI_3, kVK_ANSI_4,
+    kVK_ANSI_5, kVK_ANSI_6, kVK_ANSI_7, kVK_ANSI_8, kVK_ANSI_9,
+
+    kVK_ANSI_Grave,
+    kVK_ANSI_Equal,
+    kVK_ANSI_Minus,
+    kVK_ANSI_RightBracket,
+    kVK_ANSI_LeftBracket,
+    kVK_ANSI_Quote,
+    kVK_ANSI_Semicolon,
+    kVK_ANSI_Backslash,
+    kVK_ANSI_Comma,
+    kVK_ANSI_Slash,
+    kVK_ANSI_Period,
+]
+
+fileprivate let keyToCode = [
+    "F1": kVK_F1,
+    "F2": kVK_F2,
+    "F3": kVK_F3,
+    "F4": kVK_F4,
+    "F5": kVK_F5,
+    "F6": kVK_F6,
+    "F7": kVK_F7,
+    "F8": kVK_F8,
+    "F9": kVK_F9,
+    "F10": kVK_F10,
+    "F11": kVK_F11,
+    "F12": kVK_F12,
+    "F13": kVK_F13,
+    "F14": kVK_F14,
+    "F15": kVK_F15,
+    "F16": kVK_F16,
+    "F17": kVK_F17,
+    "F18": kVK_F18,
+    "F19": kVK_F19,
+    "F20": kVK_F20,
+
+    "PAD.": kVK_ANSI_KeypadDecimal,
+    "PAD*": kVK_ANSI_KeypadMultiply,
+    "PAD+": kVK_ANSI_KeypadPlus,
+    "PAD/": kVK_ANSI_KeypadDivide,
+    "PAD-": kVK_ANSI_KeypadMinus,
+    "PAD=": kVK_ANSI_KeypadEquals,
+    "PAD0": kVK_ANSI_Keypad0,
+    "PAD1": kVK_ANSI_Keypad1,
+    "PAD2": kVK_ANSI_Keypad2,
+    "PAD3": kVK_ANSI_Keypad3,
+    "PAD4": kVK_ANSI_Keypad4,
+    "PAD5": kVK_ANSI_Keypad5,
+    "PAD6": kVK_ANSI_Keypad6,
+    "PAD7": kVK_ANSI_Keypad7,
+    "PAD8": kVK_ANSI_Keypad8,
+    "PAD9": kVK_ANSI_Keypad9,
+    "PAD_CLEAR": kVK_ANSI_KeypadClear,
+    "PAD_ENTER": kVK_ANSI_KeypadEnter,
+
+    "RETURN": kVK_Return,
+    "TAB": kVK_Tab,
+    "SPACE": kVK_Space,
+    "DELETE": kVK_Delete,
+    "ESCAPE": kVK_Escape,
+    "HELP": kVK_Help,
+    "HOME": kVK_Home,
+    "PAGE_UP": kVK_PageUp,
+    "PAGE_DOWN": kVK_PageDown,
+    "FORWARD_DELETE": kVK_ForwardDelete,
+    "END": kVK_End,
+    "LEFT": kVK_LeftArrow,
+    "RIGHT": kVK_RightArrow,
+    "UP": kVK_UpArrow,
+    "DOWN": kVK_DownArrow,
+]
+
 open class KeyCodeHelper {
-    fileprivate static let relocatableKeyCodes = [
-        kVK_ANSI_A, kVK_ANSI_B, kVK_ANSI_C, kVK_ANSI_D, kVK_ANSI_E,
-        kVK_ANSI_F, kVK_ANSI_G, kVK_ANSI_H, kVK_ANSI_I, kVK_ANSI_J,
-        kVK_ANSI_K, kVK_ANSI_L, kVK_ANSI_M, kVK_ANSI_N, kVK_ANSI_O,
-        kVK_ANSI_P, kVK_ANSI_Q, kVK_ANSI_R, kVK_ANSI_S, kVK_ANSI_T,
-        kVK_ANSI_U, kVK_ANSI_V, kVK_ANSI_W, kVK_ANSI_X, kVK_ANSI_Y,
-        kVK_ANSI_Z,
-
-        kVK_ANSI_0, kVK_ANSI_1, kVK_ANSI_2, kVK_ANSI_3, kVK_ANSI_4,
-        kVK_ANSI_5, kVK_ANSI_6, kVK_ANSI_7, kVK_ANSI_8, kVK_ANSI_9,
-
-        kVK_ANSI_Grave,
-        kVK_ANSI_Equal,
-        kVK_ANSI_Minus,
-        kVK_ANSI_RightBracket,
-        kVK_ANSI_LeftBracket,
-        kVK_ANSI_Quote,
-        kVK_ANSI_Semicolon,
-        kVK_ANSI_Backslash,
-        kVK_ANSI_Comma,
-        kVK_ANSI_Slash,
-        kVK_ANSI_Period,
-    ]
-
     fileprivate static let relocatableKeys: [String: Int] = {
         var keys = [String: Int]()
 
@@ -68,65 +126,7 @@ open class KeyCodeHelper {
             return keyCode
         }
 
-        switch key.uppercased() {
-        case "F1": return kVK_F1
-        case "F2": return kVK_F2
-        case "F3": return kVK_F3
-        case "F4": return kVK_F4
-        case "F5": return kVK_F5
-        case "F6": return kVK_F6
-        case "F7": return kVK_F7
-        case "F8": return kVK_F8
-        case "F9": return kVK_F9
-        case "F10": return kVK_F10
-        case "F11": return kVK_F11
-        case "F12": return kVK_F12
-        case "F13": return kVK_F13
-        case "F14": return kVK_F14
-        case "F15": return kVK_F15
-        case "F16": return kVK_F16
-        case "F17": return kVK_F17
-        case "F18": return kVK_F18
-        case "F19": return kVK_F19
-        case "F20": return kVK_F20
-
-        case "PAD.": return kVK_ANSI_KeypadDecimal
-        case "PAD*": return kVK_ANSI_KeypadMultiply
-        case "PAD+": return kVK_ANSI_KeypadPlus
-        case "PAD/": return kVK_ANSI_KeypadDivide
-        case "PAD-": return kVK_ANSI_KeypadMinus
-        case "PAD=": return kVK_ANSI_KeypadEquals
-        case "PAD0": return kVK_ANSI_Keypad0
-        case "PAD1": return kVK_ANSI_Keypad1
-        case "PAD2": return kVK_ANSI_Keypad2
-        case "PAD3": return kVK_ANSI_Keypad3
-        case "PAD4": return kVK_ANSI_Keypad4
-        case "PAD5": return kVK_ANSI_Keypad5
-        case "PAD6": return kVK_ANSI_Keypad6
-        case "PAD7": return kVK_ANSI_Keypad7
-        case "PAD8": return kVK_ANSI_Keypad8
-        case "PAD9": return kVK_ANSI_Keypad9
-        case "PAD_CLEAR": return kVK_ANSI_KeypadClear
-        case "PAD_ENTER": return kVK_ANSI_KeypadEnter
-
-        case "RETURN": return kVK_Return
-        case "TAB": return kVK_Tab
-        case "SPACE": return kVK_Space
-        case "DELETE": return kVK_Delete
-        case "ESCAPE": return kVK_Escape
-        case "HELP": return kVK_Help
-        case "HOME": return kVK_Home
-        case "PAGE_UP": return kVK_PageUp
-        case "PAGE_DOWN": return kVK_PageDown
-        case "FORWARD_DELETE": return kVK_ForwardDelete
-        case "END": return kVK_End
-        case "LEFT": return kVK_LeftArrow
-        case "RIGHT": return kVK_RightArrow
-        case "UP": return kVK_UpArrow
-        case "DOWN": return kVK_DownArrow
-
-        default: return 0
-        }
+        return keyToCode[key.uppercased()] ?? 0
     }
 
     open static func modifierFlagsForString(modifiers: [String]) -> Int {
