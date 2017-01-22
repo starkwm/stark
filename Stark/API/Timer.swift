6f16df5af503dc8ebda707b12ba4c6476376818e
Tom Bell <tomb@tomb.io>
2017-01-22 04:03:14 +0000

    Add Timer ctor to JS exports

--- Stark/API/Timer.swift
+++ Stark/API/Timer.swift
@@ -2,13 +2,15 @@ import AppKit
 import JavaScriptCore
 
 @objc protocol TimerJSExport: JSExport {
+    init(interval: TimeInterval, repeats: Bool, callback: JSValue)
+
     func stop()
 }
 
 open class Timer: Handler, TimerJSExport, HashableJSExport {
     fileprivate var timer: Foundation.Timer?
 
-    init(interval: TimeInterval, repeats: Bool, callback: JSValue) {
+    public required init(interval: TimeInterval, repeats: Bool, callback: JSValue) {
         super.init()
 
         timer = Foundation.Timer.scheduledTimer(timeInterval: interval, target: self, selector: #selector(timerDidFire), userInfo: nil, repeats: repeats)
