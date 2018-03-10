2b0bc8681182fcce42290aba6e40e5a640fcf2ae
Tom Bell <tomb@tomb.io>
2018-03-10 19:50:42 +0000

    Add spaces for readability

--- Stark/API/Exports/Application+JSExport.swift
+++ Stark/API/Exports/Application+JSExport.swift
@@ -16,17 +16,22 @@ protocol ApplicationJSExport: JSExport {
     static func focused() -> Application?
 
     var name: String { get }
+
     var bundleId: String { get }
     var processId: pid_t { get }
+
     var isActive: Bool { get }
     var isHidden: Bool { get }
     var isTerminated: Bool { get }
 
     func windows() -> [Window]
     func windows(_ options: [String: AnyObject]) -> [Window]
+
     func activate() -> Bool
     func focus() -> Bool
+
     func show() -> Bool
     func hide() -> Bool
+
     func terminate() -> Bool
 }
