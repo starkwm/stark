c7c27cffd686225d3de1fa5df94a5a608dc37395
Tom Bell <tomb@tomb.io>
2019-10-08 22:28:50 +0100

    Revert "Rename function to fn"
    This reverts commit 58b3e3087c924c0d145e20539a8c95f66c1a90b0.

--- Stark/Source/API/Handler.swift
+++ Stark/Source/API/Handler.swift
@@ -16,8 +16,8 @@ public class Handler: NSObject {
                 LogHelper.log(message: String(format: "JavaScript exception: %@", exception!))
             }
 
-            let fn = JSValue(object: callback, in: scope)
-            _ = fn?.call(withArguments: arguments)
+            let function = JSValue(object: callback, in: scope)
+            _ = function?.call(withArguments: arguments)
         }
     }
 
