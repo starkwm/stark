8ca10266a83b0f3e21689125f6d414a15f1a77f7
Tom Bell <tomb@tomb.io>
2019-10-08 22:28:44 +0100

    Update swiftlint.yml to ignore image object literals

--- Stark/Source/AppDelegate.swift
+++ Stark/Source/AppDelegate.swift
@@ -32,7 +32,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     func setupStatusItem() {
-        // swiftlint:disable:next object_literal
         let image = NSImage(named: "StatusItemIcon")
         image?.isTemplate = true
 
