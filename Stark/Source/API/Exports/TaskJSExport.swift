3cff56e92ee3f120e56a6f4e3c159f593d85fd6d
Tom Bell <tomb@tomb.io>
2019-10-14 21:25:51 +0100

    Add initial TaskJSExport

---
+++ Stark/Source/API/Exports/TaskJSExport.swift
@@ -0,0 +1,11 @@
+import AppKit
+import JavaScriptCore
+
+@objc
+protocol TaskJSExport: JSExport {
+    init(path: String, arguments: [String]?, callback: JSValue)
+
+    var id: Int { get }
+
+    var status: Int { get }
+}
