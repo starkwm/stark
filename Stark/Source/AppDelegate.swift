f56bdb3f1e0d75a0963b916ba2f7571528ccc336
Tom Bell <tomb@tomb.io>
2021-03-03 19:38:18 +0000

    Move observer classes into directory

--- Stark/Source/AppDelegate.swift
+++ Stark/Source/AppDelegate.swift
@@ -2,15 +2,10 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    var observer: RunningAppsObserver?
-
     let statusItem = StarkStatusItem()
 
     func applicationDidFinishLaunching(_: Notification) {
-        observer = RunningAppsObserver()
-
         askForAccessibilityIfNeeded()
-
         statusItem.setup()
 
         NotificationCenter.default.post(name: Notification.Name(rawValue: starkDidStartLaunch), object: self)
@@ -18,11 +13,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func askForAccessibilityIfNeeded() {
         let options = [kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String: true]
-
-        if AXIsProcessTrustedWithOptions(options as CFDictionary?) {
-            return
-        }
-
-        NSApp.terminate(nil)
+        AXIsProcessTrustedWithOptions(options as CFDictionary?)
     }
 }
