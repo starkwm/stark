2980260ef40755640cd727bba269195b814f6fc9
Tom Bell <tomb@tomb.io>
2021-03-08 20:16:14 +0000

    Add running apps observer back in

--- Stark/Source/AppDelegate.swift
+++ Stark/Source/AppDelegate.swift
@@ -2,11 +2,15 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+    var observer: RunningAppsObserver?
+
     let statusItem = StarkStatusItem()
 
     func applicationDidFinishLaunching(_: Notification) {
         askForAccessibilityIfNeeded()
         statusItem.setup()
+
+        observer = RunningAppsObserver()
 
         NotificationCenter.default.post(name: Notification.Name(rawValue: starkDidStartLaunch), object: self)
     }
