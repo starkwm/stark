45e0fe6edcd72f3cc69f334380806cc979e1f4e6
Tom Bell <tomb@tomb.io>
2024-03-31 14:21:39 +0100

    Refactor Stark class

--- Stark/Source/API/Stark.swift
+++ Stark/Source/API/Stark.swift
@@ -2,13 +2,13 @@ import AppKit
 import JavaScriptCore
 
 public class Stark: NSObject, StarkJSExport {
+  private var config: Config
+  private var context: Context
+
   init(config: Config, context: Context) {
     self.config = config
     self.context = context
   }
-
-  private var config: Config
-  private var context: Context
 
   public func log(_ message: String) {
     LogHelper.log(message: message)
