975271f5244ccdb23ac083939f533a50f2c9b0d8
Tom Bell <tomb@tomb.io>
2024-04-12 02:05:53 +0100

    Remove superfluous new lines

--- Stark/Source/Extensions/Logger+Extension.swift
+++ Stark/Source/Extensions/Logger+Extension.swift
@@ -4,8 +4,6 @@ extension Logger {
   private static let subsystem = Bundle.main.bundleIdentifier!
 
   static let config = Logger(subsystem: subsystem, category: "Config")
-
   static let launchAgent = Logger(subsystem: subsystem, category: "LaunchAgentHelper")
-
   static let javascript = Logger(subsystem: subsystem, category: "JavaScript")
 }
