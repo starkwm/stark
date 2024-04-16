045efb8a1aef12ac63f8dd17a3a293837487a514
Tom Bell <tomb@tomb.io>
2024-04-16 19:06:19 +0100

    Add Events model

---
+++ Stark/Notifications/Events.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+enum EventType: String, CaseIterable {
+  case applicationLaunched
+  case applicationTerminated
+  case applicationFrontSwitched
+
+  case windowCreated
+  case windowDestroyed
+  case windowFocused
+  case windowMoved
+  case windowResized
+  case windowMinimized
+  case windowDeminimized
+  case windowTitleChanged
+
+  case spaceChanged
+}
+
+let events = EventType.allCases.reduce([EventType: Notification.Name]()) {
+  (dict, event) -> [EventType: Notification.Name] in
+  var dict = dict
+  dict[event] = Notification.Name(event.rawValue)
+  return dict
+}
