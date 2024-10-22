756b5498670a38eab3f2a856f6835cc583aeac57
Tom Bell <tomb@tombell.dev>
2024-10-22 21:10:51 +0100

    Remove logger extensions

--- Stark/Extensions/Logger+Extension.swift
+++
@@ -1,8 +0,0 @@
-import OSLog
-
-extension Logger {
-  private static let subsystem = Bundle.main.bundleIdentifier!
-
-//  static let main = Logger(subsystem: subsystem, category: "main")
-//  static let javascript = Logger(subsystem: subsystem, category: "javascript")
-}
