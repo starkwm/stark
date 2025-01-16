2c3be0a34ea16dac55b7c20a7d6c62616dfdad28
Tom Bell <tomb@tombell.dev>
2025-01-16 19:40:05 +0000

    Refactor Modifier enum

--- Stark/Models/Modifier.swift
+++ Stark/Models/Modifier.swift
@@ -1,37 +1,23 @@
 import Carbon
 
-public let modifierIdentifiers = [
-  "shift",
-  "ctrl", "control",
-  "alt", "opt", "option",
-  "cmd", "command",
-  "hyper",
-]
-
 public enum Modifier {
+  private static let keyFlags: [String: Int] = [
+    "shift": shiftKey,
+    "ctrl": controlKey, "control": controlKey,
+    "alt": optionKey, "opt": optionKey, "option": optionKey,
+    "cmd": cmdKey, "command": cmdKey,
+    "hyper": cmdKey | optionKey | shiftKey | controlKey,
+  ]
+
   public static func flags(for modifiers: [String]) -> UInt32 {
     let mods = Set(modifiers.map { $0.lowercased() })
 
     var flags = 0
 
-    if !mods.intersection(Set(["shift"])).isEmpty {
-      flags |= shiftKey
-    }
-
-    if !mods.intersection(Set(["ctrl", "control"])).isEmpty {
-      flags |= controlKey
-    }
-
-    if !mods.intersection(Set(["alt", "opt", "option"])).isEmpty {
-      flags |= optionKey
-    }
-
-    if !mods.intersection(Set(["cmd", "command"])).isEmpty {
-      flags |= cmdKey
-    }
-
-    if mods.contains("hyper") {
-      flags |= cmdKey | optionKey | shiftKey | controlKey
+    for mod in mods {
+      if let keyFlag = keyFlags[mod] {
+        flags |= keyFlag
+      }
     }
 
     return UInt32(flags)
