b324ff03ad3184fb128de9320af4cc832282866a
Tom Bell <tomb@tombell.dev>
2025-07-12 23:29:50 +0100

    Remove String extension and inline the code

--- Stark/Extensions/String+Extension.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-extension String {
-  init(_ fourCharCode: FourCharCode) {
-    self = NSFileTypeForHFSTypeCode(fourCharCode).trimmingCharacters(in: CharacterSet(charactersIn: "'"))
-  }
-}
